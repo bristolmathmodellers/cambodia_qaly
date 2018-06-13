7ccfeb46bc55b7503a091203f3ae8bda967745b2
Josephine Walker <jw12513@it017400.users.bris.ac.uk>
2018-06-13 14:29:56 +0100

    update qalyall output

--- calculate_weights.R
+++ calculate_weights.R
@@ -4,7 +4,7 @@
 # calculate utility weights from all patients for which english is available - regardless of treatment date
 source("qalyweights.R")
 
-qaly <- data %>% dplyr::select(pat_id,redcap_event_name,vst_dt,visit_date,visit_month,visit_year,mobility=spe_pp_mobility,selfcare=spe_pp_selfcare,activity=spe_pp_activities,pain=spe_pp_pain,anxiety=spe_pp_anxiety,healthtoday=spe_pp_health,outc_tx_decision,outc_eligible,cured) %>%
+qaly <- data %>% dplyr::select(pat_id,redcap_event_name,vst_dt,visit_date,visit_month,visit_year,mobility=spe_pp_mobility,selfcare=spe_pp_selfcare,activity=spe_pp_activities,pain=spe_pp_pain,anxiety=spe_pp_anxiety,healthtoday=spe_pp_health,outc_tx_decision,outc_eligible,model,cured) %>%
   mutate(
     mobility = ifelse(mobility > 5 | is.na(mobility), NA, mobility),
     selfcare = ifelse(selfcare > 5 | is.na(selfcare), NA, selfcare),
@@ -17,7 +17,10 @@ qaly <- data %>% dplyr::select(pat_id,redcap_event_name,vst_dt,visit_date,visit_
 
 qalyweighted <- QALYweights(qaly)
 
-qalyinitial_dist <- qalyweighted %>% filter(language=="Khmer" & redcap_event_name=="initial_arm_1") %>% left_join(fstage,by="pat_id")
+qalyinitial_dist <- qalyweighted %>% filter(language=="Khmer" & redcap_event_name=="initial_arm_1") %>% left_join(fstage,by="pat_id") 
+
+
+qalyinitial2 <- qalyinitial_dist %>% select(pat_id,thai_utility,indo_utility,stage,dem_age,dem_sex,treated,started_treatment,cured=cured.x,model=model.x) %>% mutate(point="initial")
 
 qalyinitial <- qalyweighted %>% filter(language=="Khmer" & redcap_event_name=="initial_arm_1") %>% left_join(fstage,by="pat_id") %>% group_by(stage) %>% summarise(thai_utility_mean=mean(thai_utility,na.rm=T),thai_utility_median=median(thai_utility,na.rm=T),indo_utility_mean=mean(indo_utility,na.rm=T),indo_utility_median=median(indo_utility,na.rm=T),n=n(),indo_low=quantile(indo_utility,probs=0.025,na.rm=T),indo_high=quantile(indo_utility,probs=0.975,na.rm=T)) %>% filter(!is.na(stage))
 
@@ -27,7 +30,7 @@ qalyinitialcirrgroup <- qalyinitialcirr %>% group_by(cirr,time) %>% summarise(th
 
 # take mean of individuals first - change to use area under curve
 # qalytreatment <- qalyweighted %>% filter(language=="Khmer" & outc_tx_decision %in% c("C","W","M","TF")) %>% group_by(pat_id) %>% summarise(thai_utility=mean(thai_utility,na.rm=T),indo_utility=mean(indo_utility,na.rm=T),n=n()) 
-qalytreatment <- qalyweighted %>% filter(language=="Khmer" & outc_tx_decision %in% c("C","W","M","TF") & !is.na(indo_utility)) %>% group_by(pat_id) %>% arrange(pat_id,visit_date) %>% mutate(date = visit_date-first(visit_date),last_date=as.numeric(last(visit_date)-first(visit_date))) %>% summarise(thai_utility=ifelse(n()>1,trapz(date,thai_utility)/last_date,thai_utility),indo_utility=ifelse(n()>1,trapz(date,indo_utility)/last_date,indo_utility),n=n()) %>% left_join(fstage,by="pat_id") %>% filter(!is.na(stage))
+qalytreatment <- qalyweighted %>% filter(language=="Khmer" & outc_tx_decision %in% c("C","W","M","TF") & !is.na(indo_utility)) %>% group_by(pat_id) %>% arrange(pat_id,visit_date) %>% mutate(date = visit_date-first(visit_date),last_date=as.numeric(last(visit_date)-first(visit_date))) %>% summarise(thai_utility=ifelse(n()>1,trapz(date,thai_utility)/last_date,thai_utility),indo_utility=ifelse(n()>1,trapz(date,indo_utility)/last_date,indo_utility),n=n(),point="treatment") %>% left_join(fstage,by="pat_id") %>% filter(!is.na(stage))
 
 qalytreatmentgroup <- qalytreatment %>% group_by(stage) %>% summarise(thai_utility_mean=mean(thai_utility,na.rm=T),thai_utility_median=median(thai_utility,na.rm=T),indo_utility_mean=mean(indo_utility,na.rm=T),indo_utility_median=median(indo_utility,na.rm=T),n=n(),indo_low=quantile(indo_utility,probs=0.025,na.rm=T),indo_high=quantile(indo_utility,probs=0.975,na.rm=T))
 
@@ -36,7 +39,7 @@ qalytreatmentcirr$cirr[qalytreatmentcirr$cirr %in% c("F0","F1","F2","F3")] <- "p
 qalytreatmentcirrgroup <- qalytreatmentcirr %>% group_by(cirr,time) %>% summarise(thai_utility_mean=mean(thai_utility,na.rm=T),thai_utility_median=median(thai_utility,na.rm=T),indo_utility_mean=mean(indo_utility,na.rm=T),indo_utility_median=median(indo_utility,na.rm=T),n=n(),indo_low=quantile(indo_utility,probs=0.025,na.rm=T),indo_high=quantile(indo_utility,probs=0.975,na.rm=T))
 
 # includes multiple follow ups for some patients - trapz if multiples as for treatment.
-qalySVR_dist <- qalyweighted %>% filter(language=="Khmer" & outc_eligible=="FUP" & !is.na(indo_utility))  %>% group_by(pat_id) %>% arrange(pat_id,visit_date) %>% mutate(date = visit_date-first(visit_date),last_date=as.numeric(last(visit_date)-first(visit_date))) %>% summarise(thai_utility=ifelse(n()>1,trapz(date,thai_utility)/last_date,thai_utility),indo_utility=ifelse(n()>1,trapz(date,indo_utility)/last_date,indo_utility),n=n()) %>% left_join(fstage,by="pat_id") %>% filter(!is.na(stage))
+qalySVR_dist <- qalyweighted %>% filter(language=="Khmer" & outc_eligible=="FUP" & !is.na(indo_utility))  %>% group_by(pat_id) %>% arrange(pat_id,visit_date) %>% mutate(date = visit_date-first(visit_date),last_date=as.numeric(last(visit_date)-first(visit_date))) %>% summarise(thai_utility=ifelse(n()>1,trapz(date,thai_utility)/last_date,thai_utility),indo_utility=ifelse(n()>1,trapz(date,indo_utility)/last_date,indo_utility),n=n(),point="SVR") %>% left_join(fstage,by="pat_id") %>% filter(!is.na(stage))
 
 qalySVR <- qalySVR_dist %>% group_by(pat_id) %>% group_by(stage) %>% summarise(thai_utility_mean=mean(thai_utility,na.rm=T),thai_utility_median=median(thai_utility,na.rm=T),indo_utility_mean=mean(indo_utility,na.rm=T),indo_utility_median=median(indo_utility,na.rm=T),n=n(),indo_low=quantile(indo_utility,probs=0.025,na.rm=T),indo_high=quantile(indo_utility,probs=0.975,na.rm=T))
 
@@ -47,3 +50,6 @@ qalySVRcirr$cirr[qalySVRcirr$cirr %in% c("F0","F1","F2","F3")] <- "pre-cirrhosis
 qalySVRcirrgroup <- qalySVRcirr %>% group_by(cirr,time) %>% summarise(thai_utility_mean=mean(thai_utility,na.rm=T),thai_utility_median=median(thai_utility,na.rm=T),indo_utility_mean=mean(indo_utility,na.rm=T),indo_utility_median=median(indo_utility,na.rm=T),n=n(),indo_low=quantile(indo_utility,probs=0.025,na.rm=T),indo_high=quantile(indo_utility,probs=0.975,na.rm=T))
 
 qalycirr <- bind_rows(qalyinitialcirrgroup,qalytreatmentcirrgroup,qalySVRcirrgroup)
+
+qalyall <- bind_rows(qalyinitial2,qalytreatment,qalySVR_dist) 
+# save(qalyall,file="~/dat/Cambodia_data/qalyall.RData")
