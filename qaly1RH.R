27e7c47785b24da034206b7cb2ac3e221e363f29
rh14221 <reubenahastings@gmail.com>
2018-06-25 16:14:48 +0100

    Lots of changes
    Lots of changes

--- qaly1RH.R
+++ qaly1RH.R
@@ -54,11 +54,15 @@ TukeyHSD(av3, which = "point")
 hist(residuals(av3)) #Not normal, right skew
 
 #BETA REGRESSION - needs to be between 0 and 1
+mydata$betas <- mydata$thai_utility - 0.001
+mydata$betas[mydata$betas <=0] <-NA
 library(betareg)
 trainingIndex <- c(1:(nrow(mydata)-1))
 trainingData <- mydata[trainingIndex, ]
 testData <- mydata[-trainingIndex, ]
-betaMod <- betareg(thai_utility ~ point + model, data = trainingData)
+betaMod <- betareg(betas ~ point + stage, data = trainingData)
+summary (betaMod) # model summary
+predict (betaMod, testData) # predict on test data
 
 #LINEAR MODEL
 library(lme4)
@@ -70,3 +74,40 @@ summary(fit)
 logLik(lm1)
 logLik(fit)
 lm2 <- glm(thai_utility ~ point + model + (1|pat_id) + (1|stage) + (1|dem_age) + (1|dem_sex), data = mydata, REML=FALSE)
+
+#MULTILEVEL MODEL
+lmer(thai_utility~point+(1|pat_id), data=mydata, REML=FALSE) #Basic model, poor fit
+lmer1 <- lmer(thai_utility~point+stage+dem_sex+dem_age+cured+(1|pat_id), data=mydata, REML=FALSE)
+coef1 <- data.frame(coef(summary(lmer1))) #Obtaining p values by using the normal approximation
+coef1$p.z <- 2 * (1 - pnorm(abs(coef1$t.value)))
+coef1
+install.packages("pbkrtest")
+library(pbkrtest)
+df.KR1 <- get_ddf_Lb(lmer1, fixef(lmer1)) #Getting the Kenward Roger-approximated df
+coef1$p.KR <- 2 * (1 - pt(abs(coef1$t.value), df.KR1)) #Getting p-values from the t-distribution using the t-values and approximated df
+coef1
+lmer2 <- lmer(thai_utility~point+stage+point*stage+model+dem_sex+dem_age+dem_age*stage+cured+(1|pat_id), data=mydata, REML=FALSE)
+coef2 <- data.frame(coef(summary(lmer2))) #Obtaining p values by using the normal approximation
+coef2$p.z <- 2 * (1 - pnorm(abs(coef2$t.value)))
+coef2
+df.KR2 <- get_ddf_Lb(lmer2, fixef(lmer2)) #Getting the Kenward Roger-approximated df
+coef2$p.KR <- 2 * (1 - pt(abs(coef2$t.value), df.KR2)) #Getting p-values from the t-distribution using the t-values and approximated df
+coef2
+qqnorm(residuals(lmer2))
+qqline(residuals(lmer2))
+nullmer <- thai_utility ~ (1 | pat_id)
+anova(nullmer, lmer2) # Can't compare models, not sure why
+
+#Does F stage grouping matter?
+br1 <- betareg(betas ~ point + stage, data=mydata)
+mydata$new <- as.factor(mydata$stage)
+levels(mydata$new) <- c("mild", "mild", "moderate", "moderate", "cirrhosis", "post", "post")
+br2 <- betareg(betas ~ point + new, data = mydata)
+mydata$new2 <- as.factor(mydata$stage)
+levels(mydata$new2) <- c("preC", "preC", "preC", "postC", "postC", "postC")
+br3 <- betareg(betas ~ point + new2, data = mydata)
+summary(br1)
+summary(br2)
+summary(br3)
+library(lmtest)
+lrtest(br1, br2, br3)
